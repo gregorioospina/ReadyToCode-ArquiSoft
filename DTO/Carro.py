0ff6e5ae1e97d2e10d5317c15d8641f92245d3cc
IVAN DAVID SALAZAR CARDENAS <id.salazar@sis.virtual.uniandes.edu.co>
2018-09-25 17:05:48 -0500

    El de penaloza

---
+++ DTO/Carro.py
@@ -0,0 +1,24 @@
+class Carro:
+
+	def__init__(self,pla,use,nod):
+		self.placa = pla
+		self.idUsuario = use
+		self.nombreDuenio = nod
+
+	def darPlaca(self):
+		return placa
+
+	def darIdUsuario(self):
+		return idUsuario
+
+	def darNombreDuenio(self):
+		return nombreDuenio	
+
+	def setPlaca(self, pla):
+		self.placa = pla;
+
+	def setIdUsuario(self, use):
+		self.idUsuario = use;
+
+	def setNombreDuenio(self, nod):
+		self.nombreDuenio = nod;
