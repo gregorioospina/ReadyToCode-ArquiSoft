9749bcd4e961a94bee8d4b2eddd6afa51865a2f2
oamaya724 <35939407+oamaya724@users.noreply.github.com>
2018-09-27 15:34:02 -0500

    Update consumer.py

--- Publish-Subscribe/consumer.py
+++ Publish-Subscribe/consumer.py
@@ -10,9 +10,13 @@ consumer.subscribe(pattern='reserva.*')
 producer = KafkaProducer(bootstrap_servers=['172.24.41.128:8081'], 
 				value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-
-
 for message in consumer:
-	print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-										message.offset, message.key,
-										message.value))
+    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value))
+
+
+    producer.send('reserva', {'costo': '150000', 'fechaInicio': datetime.datetime.now().strftime("%A"), 
+										'fechaCaduca': datetime.datetime.now().strftime("%A"), 'carro': 'Renault 4', 'parqueadero': '00000001',
+										'duenoParqueadero': '0000000011', 'usuarioParqueadero': '000000034'})
+    producer.flush()
